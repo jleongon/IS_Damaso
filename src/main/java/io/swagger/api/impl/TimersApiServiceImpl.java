f4c3bacfd75cd28433c1f4745b1a287208c029d0
Javier León González <jleongon@alumnos.unex.es>
2019-11-21 19:52:59 +0100

    Add files via upload

--- src/main/java/io/swagger/api/impl/TimersApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/TimersApiServiceImpl.java
@@ -5,8 +5,10 @@ import io.swagger.model.*;
 
 import io.swagger.model.InlineResponse200;
 import io.swagger.model.Timer;
+import io.swagger.v3.core.util.Json;
 
 import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 import io.swagger.api.NotFoundException;
 
@@ -18,29 +20,46 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2019-11-07T18:16:59.231Z[GMT]")public class TimersApiServiceImpl extends TimersApiService {
-    @Override
+	
+	Map<Integer, Timer> map = new HashMap<Integer, Timer>();
+	int id = 0;
+	
+	
+	@Override
     public Response addTimer(Timer body, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+		body.setIdTimer((long) id);
+		map.put(id, body);		
+        return Response.ok().entity(Integer.toString(id++)).build();
     }
     @Override
     public Response deleteTimer(Long timerID, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	if(!map.containsKey(timerID))
+    		return Response.status(404).build();
+    	map.remove(timerID);
+        return Response.ok().entity("").build();
     }
     @Override
     public Response getTimer(Long timerID, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	if(!map.containsKey(timerID))
+    		return Response.status(404).build();
+        return Response.ok().entity(Json.pretty(map.get(timerID))).build();
     }
     @Override
     public Response getTimers(SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	if( map.size() == 0)
+    		return Response.status(408).build();
+        return Response.ok().entity(Json.pretty(map.values().toArray())).build();
     }
     @Override
     public Response modifyTimer(Timer body, Long timerID, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	if(!map.containsKey(timerID))
+    		return Response.status(404).build();
+    	map.put(timerID.intValue(), body);
+        return Response.ok().entity("").build();
     }
 }
